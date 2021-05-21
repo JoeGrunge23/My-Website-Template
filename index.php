543d6881c694129df90ecd0ee4b835dc302054de
JoeGrunge23 <joerizcasiple@hotmail.com>
2021-05-22 07:28:53 +0800

    updated

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
     <link rel="stylesheet" href="css/style.css">
     <title>Joeriz Casiple</title>
 </head>
 <body>
 <nav id="nav">
     <ul>
-      <li><a href="#header">Home</a></li>
+      <li><a href="#nav">Home</a></li>
       <li><a href="#about">About</a></li>
       <li><a href="#portfolio">Portfolio</a></li>
       <li><a href="#contact">Contact</a></li>
@@ -18,8 +19,10 @@
   </nav>
   <header id="header">
     <h1>HI! <span>I'm Joeriz!</span></h1>
-    <h2>I'm a <span>Web Developer</span></h2>
-    <a href="#about">Let's Get Started!</a>
+    <h2>I'm a <span>Programmer</span></h2>
+    <a href="#about">
+      Let's Get Started! 
+    </a>
   </header>
   <article id="about"></article>
   <article id="portfolio">portfolio</article>
