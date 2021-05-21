2a8dff67e534f93dae3ed6fd7aa7facd85a76499
JoeGrunge23 <joerizcasiple@hotmail.com>
2021-05-22 04:54:16 +0800

    updated

--- index.php
+++ index.php
@@ -5,20 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/fonts.css">
-
-    <title>Document</title>
+    <title>Joeriz Casiple</title>
 </head>
 <body>
 <nav id="nav">
     <ul>
-      <li><a href="#header">Header</a></li>
+      <li><a href="#header">Home</a></li>
+      <li><a href="#about">About</a></li>
       <li><a href="#portfolio">Portfolio</a></li>
       <li><a href="#contact">Contact</a></li>
-      <li><a href="#footer">Footer</a></li>
-    </ul>
+     </ul>
   </nav>
-  <header id="header">hsseader</header>
+  <header id="header">
+    <h1>HI! <span>I'm Joeriz!</span></h1>
+    <h2>I'm a <span>Web Developer</span></h2>
+    <a href="#about">Let's Get Started!</a>
+  </header>
   <article id="about"></article>
   <article id="portfolio">portfolio</article>
   <!-- <article id="blog">blog</article> -->
