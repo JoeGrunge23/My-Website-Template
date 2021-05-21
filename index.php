67824ac948969658d7ea6e008cf7910f844b3453
JoeGrunge23 <joerizcasiple@hotmail.com>
2021-05-21 10:32:29 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fonts.css">
+
+    <title>Document</title>
+</head>
+<body>
+<nav id="nav">
+    <ul>
+      <li><a href="#header">Header</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#footer">Footer</a></li>
+    </ul>
+  </nav>
+  <header id="header">hsseader</header>
+  <article id="about"></article>
+  <article id="portfolio">portfolio</article>
+  <!-- <article id="blog">blog</article> -->
+  <article id="contact">contact</article>
+<footer id="footer">footer</footer>
+</body>
+</html>
